a2919aa05ab573d5e21d64190496e7160434f4d8
Apurva Nandan <apurva3000@gmail.com>
2015-09-09 11:18:15 +0000

    autodetect cluster url

--- sparkles/modules/utils/runner.py
+++ sparkles/modules/utils/runner.py
@@ -11,6 +11,7 @@ from swiftclient.service import SwiftService, SwiftUploadObject
 import json
 import warnings
 import shutil
+import socket
 
 
 class SparkRunner(object):
@@ -41,6 +42,7 @@ class SparkRunner(object):
                     warnings.warn('The metadata was not found on backend, creating new! If this is not your first time usage after installing the library, please consult with the support team before proceeding!', RuntimeWarning)
 
         dburi = config['DATABASE_URI']
+        self.clusterUrl = "spark://" + socket.gethostname() + ':' + config['CLUSTER_PORT']
         self.session = config_to_db_session(dburi, Base)
         self.config = config
         self.configpath = configpath
@@ -156,11 +158,11 @@ class SparkRunner(object):
 
                 helperpath = dirname(dirname(os.path.abspath(__file__)))
                 if(features is None):
-                    call(["/opt/spark/bin/pyspark", out_file, "--master", self.config['CLUSTER_URL'], helperpath, params, filepaths])
+                    call(["/opt/spark/bin/pyspark", out_file, "--master", self.clusterUrl, helperpath, params, filepaths])
                 else:
                     features['configpath'] = self.configpath  # Pass the configpath as a default parameter
                     features = json.dumps(features)
-                    call(["/opt/spark/bin/pyspark", out_file, "--master", self.config['CLUSTER_URL'], helperpath, params, filepaths, features])
+                    call(["/opt/spark/bin/pyspark", out_file, "--master", self.cluserUrl, helperpath, params, filepaths, features])
             else:
                 raise RuntimeError("Analysis module not found")
 
@@ -178,7 +180,7 @@ class SparkRunner(object):
             path = dirname(dirname(os.path.abspath(__file__)))
             configpath = self.configpath
             originalpaths = json.dumps(inputfiles)
-            call(["/opt/spark/bin/pyspark", path + "/data_import.py", "--master", self.config['CLUSTER_URL'], originalpaths, description, details, userdatadir, configpath])
+            call(["/opt/spark/bin/pyspark", path + "/data_import.py", "--master", self.clusterUrl, originalpaths, description, details, userdatadir, configpath])
 
         else:
             raise RuntimeError("Please ensure inputfiles is not None or empty")
